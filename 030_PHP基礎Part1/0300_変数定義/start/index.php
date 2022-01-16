dd1ae72af4a01e38fc62e89914df45f231a76081
kokihara <nov17hk@gmail.com>
2022-01-16 22:11:19 +0900

    0400まで完了

--- 030_PHP基礎Part1/0300_変数定義/start/index.php
+++ 030_PHP基礎Part1/0300_変数定義/start/index.php
@@ -1,1 +1,9 @@
-<?php 
+<?php
+/* 重複した値は変数で取り出してあげる
+    命名規則にはアンダーバーを使用する「スネークケース」と
+    「キャメル（ラクダ）ケース」が存在する
+*/
+    $person_name = 'Tim';
+    echo 'hello,'.$person_name.'<br>';
+    echo 'bye,'.$person_name;
+?>
--- 030_PHP基礎Part1/0400_文字列の操作/start/index.php
+++ 030_PHP基礎Part1/0400_文字列の操作/start/index.php
@@ -1,1 +1,11 @@
-<?php 
+<?php
+/* PHPの場合は、[.]を使用して文字列を結合する
+''と""の大きな違いは、ダブルクオーテーション内だと変数は[.]は必要ない。
+文字列の中で変数を使用する場合は{}を使用する
+ダブルクオーテーション内ではエスケープシーケンス(\nなど)が使用できる。
+https://www.php.net/manual/ja/language.types.string.php#language.types.string.syntax.double
+
+*/
+$name = 'Bob';
+echo "hello, $name\n\n\n";
+?>
