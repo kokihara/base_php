2527975bbeaea32e4ffdf49fcabcbadb8239adba
kokihara <nov17hk@gmail.com>
2022-01-16 22:35:05 +0900

    0600完了

--- 030_PHP基礎Part1/0600_データ型について知ろう/start/index.php
+++ 030_PHP基礎Part1/0600_データ型について知ろう/start/index.php
@@ -1,1 +1,16 @@
-<?php 
+<?php
+/* データ型とは
+参考URL
+https://www.php.net/manual/ja/language.types.php
+*/
+$i = 1 ;
+$b = true;
+$str = 'hello';
+// var_dump($b);
+// データ型の確認方法　var_dump
+// 異なる型は自動的に変換される
+// 自動的に片方の型に統一されて表示される
+// echo $i + (int)$b ;
+// 型の取り扱い型の注意点
+// 文字列型にすると思うように結果が得られないことがあるので注意する
+var_dump($i === '1');
