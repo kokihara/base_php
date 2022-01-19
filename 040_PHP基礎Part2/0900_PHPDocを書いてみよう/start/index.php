ccb494fa06e5ee24d80dc74417ed0f892111dd67
kokihara <nov17hk@gmail.com>
2022-01-19 22:36:00 +0900

    PHP基礎２完了

--- 040_PHP基礎Part2/0900_PHPDocを書いてみよう/start/index.php
+++ 040_PHP基礎Part2/0900_PHPDocを書いてみよう/start/index.php
@@ -1,6 +1,26 @@
 <?php
+/**
+ * 税率計算のための関数を記述するためのファイル
+ * アノテーションが使用できる　https://zonuexe.github.io/phpDocumentor2-ja/references/phpdoc/index.html
+ * https://www.phpdoc.org/
+ * @author harakoki
+ * @since 1.0.0
+ */
+
+
+/**
+ * 税込み金額を取得する関数
+ *
+ * @param int $base_price 価格
+ * @param float $tax_rate 税率
+ *
+ * @return int 税込み金額
+ * @see https://example.com
+ */
 function with_tax($base_price, $tax_rate = 0.1) {
     $sum_price = $base_price + ($base_price * $tax_rate);
     $sum_price = round($sum_price);
     return $sum_price;
 }
+$price = with_tax(1000,0.08);
+echo $price;
