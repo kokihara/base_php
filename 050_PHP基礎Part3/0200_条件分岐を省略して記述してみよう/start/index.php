bc8501f37f5b26500b2ddc2e72e2fe8d97a14e44
kokihara <nov17hk@gmail.com>
2022-01-20 22:24:30 +0900

    条件分岐の省略

--- 050_PHP基礎Part3/0200_条件分岐を省略して記述してみよう/start/index.php
+++ 050_PHP基礎Part3/0200_条件分岐を省略して記述してみよう/start/index.php
@@ -1,1 +1,21 @@
 <?php
+/**
+ * 条件分岐を省略して記入する
+ *
+ *
+ */
+$array = [
+    'key' => 10,
+];
+
+if (isset($array['key'])) {
+    $array['key'] *= 10;
+} else {
+    $array['key'] = 1;
+}
+// null合体演算子
+$array['key'] = $array['key'] ?? 1;
+
+// 三項演算子
+// $array['key'] = isset($array['key']) ? $array['key'] * 10 : 1;
+echo $array['key'];
