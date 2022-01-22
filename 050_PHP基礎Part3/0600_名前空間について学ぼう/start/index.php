81b00f0e25464014f22ce73c770eec62ea1b6d64
kokihara <nov17hk@gmail.com>
2022-01-22 19:11:16 +0900

    名前空間について

--- 050_PHP基礎Part3/0600_名前空間について学ぼう/start/index.php
+++ 050_PHP基礎Part3/0600_名前空間について学ぼう/start/index.php
@@ -1,12 +1,15 @@
 <?php
-if(!defined('TAX_RATE')) {
-    define('TAX_RATE', 0.1);
-}
-    
-function with_tax($base_price, $tax_rate = TAX_RATE) {
-    $sum_price = $base_price + ($base_price * $tax_rate);
-    $sum_price = round($sum_price);
-    return $sum_price;
-}
-$price = with_tax(1000, 0.08);
-echo $price;
+/**
+ * 名前空間について学ぶ
+ *
+ * コードの格納場所
+ */
+
+require_once 'lib.php';
+// use function lib\with_tax;
+// use const lib\TAX_RATE;
+
+// バックスラッシュ名前空間で関数を宣言する
+$price = \lib\with_tax(1000, 0.08);
+// echo $price;
+echo \lib\TAX_RATE;
