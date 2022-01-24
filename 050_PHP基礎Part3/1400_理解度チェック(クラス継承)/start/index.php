063a9b31a868c41817dcaa645a9c796ed072ae49
kokihara <nov17hk@gmail.com>
2022-01-24 21:33:29 +0900

    クラス継承完了

--- 050_PHP基礎Part3/1400_理解度チェック(クラス継承)/start/index.php
+++ 050_PHP基礎Part3/1400_理解度チェック(クラス継承)/start/index.php
@@ -1,16 +1,16 @@
 <?php
 /**
  * 理解度チェック（クラス継承）
- * 
+ *
  * MyFileWriterを継承してログをファイルに書き込む
  * LogWriterクラスを作成してみてください。
- * 
+ *
  * LogWriterクラスではformatメソッドにより
  * 出力したい文字列に時間を設定できるものとします。
- * 
+ *
  * 例）
  * 2021/04/04 23:02:59 これはログです。
- * 
+ *
  * ヒント）
  * MyFileWriterのメソッドも一部変更する
  * 必要があります。
@@ -28,25 +28,33 @@ class MyFileWriter
 
     function append($content)
     {
-        $this->content .= $content;
+        $this->content .= $this->format($content);
         return $this;
     }
 
-    function newline()
-    {
-        return $this->format(PHP_EOL);
-    }
-
-    function format($content)
+    protected function format($content)
     {
         return $content;
     }
 
-    function commit($flag = null)
+    function newline()
     {
+        $this->content .= PHP_EOL;
+        return $this;
+    }
+
+    function commit($flag = null) {
         file_put_contents($this->filename, $this->content, $flag);
         $this->content = '';
         return $this;
+    }
+}
+
+class LogWriter extends MyFileWriter{
+    protected function format($content){
+        $time_str = date('Y/m/d H:i:s');
+        return sprintf('%s %s', $time_str, $content);
+
     }
 }
 
@@ -56,8 +64,6 @@ class MyFileWriter
 */
 $time_str = date('Y/m/d H:i:s');
 sprintf('%s %s', $time_str, '文字列');
-
-/* クラスの呼び出し方は以下のようにするものとします。
 
 $info = new LogWriter('info.log');
 $error = new LogWriter('error.log');
@@ -69,5 +75,3 @@ $info->append('これは通常ログです。')
 $error->append('これはエラーログです。')
     ->newline()
     ->commit(LogWriter::APPEND);
-
-*/
